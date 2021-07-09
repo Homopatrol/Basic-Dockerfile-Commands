f11268bffd834e9358d5c9e8dfbc0bd961719465
Homopatrol <58219367+Homopatrol@users.noreply.github.com>
2021-07-09 13:10:12 +0100

    Update hello_bash.sh

--- hello_bash.sh
+++ hello_bash.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-option="${OPTARG}"
+read -r -p "Hi,is this your first dockerfile? [y/N]" response
 
-printf "\nHi,is this your first dockerfile?(Y\n)\n"
+printf "\n"
 
-if [ "$option" != "Y" ] |  [ "$option" != "y" ]; then
+if [ "$response" = "Y" ] | [ "$response" = "y" ]; then
   printf "\nCongrats :)\n"
+  exit 1
+elseif [ "$response" = "N" ] | [ "$response" = "n" ]; then
+  printf "\nYour already a pro then?\n"
+  exit 1
+else
+  printf "\nPlease input a valid character [y/N]\n"
   exit 1
 fi
