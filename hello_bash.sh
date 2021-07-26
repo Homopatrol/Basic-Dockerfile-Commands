0557962dfcda6d6756649a7d4c58d090a14ccbf5
Pandora <58219367+Homopatrol@users.noreply.github.com>
2021-07-26 13:11:14 +0100

    Update hello_bash.sh

--- hello_bash.sh
+++ hello_bash.sh
@@ -1,16 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 read -r -p "Hi,is this your first dockerfile? [y/N]" response
 
 printf "\n"
 
-if [ "$response" = "Y" ] | [ "$response" = "y" ]; then
+if [[ "$response" = "Y" ]] || [[ "$response" = "y" ]]; then
   printf "\nCongrats :)\n"
+  printf "\n"
   exit 1
-elseif [ "$response" = "N" ] | [ "$response" = "n" ]; then
+elif [[ "$response" = "N" ]] || [[ "$response" = "n" ]]; then
   printf "\nYour already a pro then?\n"
+  printf "\n"
   exit 1
 else
   printf "\nPlease input a valid character [y/N]\n"
+  printf "\n"
   exit 1
 fi
